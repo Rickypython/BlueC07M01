53c49cc491d7064c8bd7b5b5f5cba9cefc9bb19c
Luiz Ricardo <87348741+Rickypython@users.noreply.github.com>
2021-08-18 20:24:57 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+//console.log('Olá,mundo');
+//prompt('Digite seu nome');
+//let usuario = 'Ricardo'
+//console.log (usuario);
+let aluno = prompt('Informe o nome do Aluno');
+let serie = prompt('Informe a serie do Aluno');
+
+console.log('O '+aluno+' está inscrito na série '+serie);
